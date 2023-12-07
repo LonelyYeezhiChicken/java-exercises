05c899fc4da716e5544cc7bd1c3329fceeaa066c
unknown <hibary.taiwan@gmail.com>
2023-12-07 22:25:04 +0800

    feat: update string

---
+++ javaString/equalsIgnoreCaseString.java
@@ -0,0 +1,12 @@
+package javaString;
+
+public class equalsIgnoreCaseString {
+    public static void main(String[] args) {
+        // 宣告字串
+        String hi1 = "hi hi";
+        String hi2 = "Hi Hi";
+
+        // 印出結果
+        System.out.println(hi1.equalsIgnoreCase(hi2));
+    }
+}
